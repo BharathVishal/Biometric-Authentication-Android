d8aea246e9c298cd207402b9f350ae8395c740b2
Bharath Vishal G <bharathvishalg@gmail.com>
2024-09-17 11:45:29 +0530

    dev commit
    - Upgraded gradle plugin to 8.6.0.
    - Upgraded target and compile SDK to Android 15 (API Level 35).
    - Updated all internal dependencies.

---
+++ app/build.gradle.kts
@@ -0,0 +1,91 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.compose.compiler)
+}
+
+android {
+    namespace = "com.bharathvishal.biometricauthentication"
+    compileSdk = 35
+    defaultConfig {
+        applicationId = "com.bharathvishal.biometricauthentication"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 80
+        versionName = "3.7"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+    buildFeatures {
+        viewBinding = true
+        compose = true
+    }
+    buildTypes {
+        debug {
+            isDebuggable = true
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+        release {
+            isDebuggable = false
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.15"
+    }
+
+    packaging {
+        jniLibs {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.legacy.legacysupport)
+    implementation(libs.jetbrains.kotlinx.couroutine)
+    implementation(libs.coil.kt)
+    implementation(libs.jetbrains.kotlin.stdlib)
+    implementation(libs.google.android.material)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    implementation(libs.glide)
+    annotationProcessor(libs.glide.compiler)
+    androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.androidx.cardview)
+    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.material3.windowsize)
+    implementation(libs.androidx.compose.material3.windowsize)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.core.core.splashscreen)
+    implementation(libs.androidx.compose.material.material.icons)
+    implementation(libs.google.accompanist.accompanist)
+    implementation(libs.androidx.biometric)
+}
+
---
+++ settings.gradle.kts
@@ -0,0 +1,26 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven {
+            setUrl("https://jitpack.io")
+        }
+    }
+}
+
+rootProject.name = "Biometric Authentication Android"
+include(":app")
