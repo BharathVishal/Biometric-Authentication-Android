7f5db48af773e35dfb339cdeeead146e3baa9423
Bharath Vishal G <bharathvishalg@gmail.com>
2026-01-10 19:35:24 +0530

    dev commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
@@ -14,8 +16,8 @@ android {
         }
         minSdk = 23
         targetSdk = 36
-        versionCode = 93
-        versionName = "4.1"
+        versionCode = 94
+        versionName = "4.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     buildFeatures {
@@ -44,9 +46,10 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-
-    kotlinOptions {
-        jvmTarget = "17"
+    kotlin {
+        compilerOptions {
+            jvmTarget = JvmTarget.JVM_17
+        }
     }
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.15"
